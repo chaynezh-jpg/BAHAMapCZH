52bbc0d73613f46779dbe956802ed8d406ef10b6
chaynezh-jpg <chaynezh@berkeley.edu>
2025-10-27 17:24:08 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2hheW5lemgiLCJhIjoiY21oOXJ1eTQ5MDk3bjJpb2o4ZHFtNXB2MiJ9.-81gnYO-U7epgEZuXTGaRw';
+
+const map = new mapboxgl.Map({
+  container: 'map', 
+  style: 'mapbox://styles/chaynezh/cmh9s3tfp00qk01r5bkstfkbz', 
+  center: [-122.3, 37.87],
+  zoom: 12
+    });
